23b59eececaf7525e4e35215fa6e3d781257f571
Camilo Caroso <camilitosamuel@hotmail.com>
2019-11-16 18:06:51 -0500

    blackjack

--- script.js
+++ script.js
@@ -276,6 +276,8 @@ function reset(){
     playerRow.style.display = "none";
     const elements = document.getElementsByClassName("card");
     while (elements.length > 0) elements[0].remove();
+    deck = [];
+    deck = ["hA","h2","h3","h4","h5","h6","h7","h8","h9","h10","hJ","hQ","hK","cA","c2","c3","c4","c5","c6","c7","c8","c9","c10","cJ","cQ","cK","dA","d2","d3","d4","d5","d6","d7","d8","d9","d10","dJ","dQ","dK","sA","s2","s3","s4","s5","s6","s7","s8","s9","s10","sJ","sQ","sK"]
     totalNumberD = 0;
     totalNumberP = 0;
     playerAmount.style.background = "";
